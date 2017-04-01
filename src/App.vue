a4b7bacd74d35f61c4907372bc6afb24ef8c4ea3
Vijay Dharap <dharapvj@gmail.com>
2017-04-01 12:07:17 +0530

    Purposefully badly written version to demonstrate ill effects of using ES6 modules without any proper thoughts.
    Subsequently, we will show to analyze and optimize things one by one.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+html {
+    font-size: 62.5%;
+}
+body {
+    font-size: 1.6rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin: 2.0rem;
+    /*background-image: linear-gradient(to bottom, #f9f9ff 0%,#cbebff 69%,#a1dbff 100%)*/
+}
+
+p{
+    font-size: 1.4rem;
+}
+
+</style>
